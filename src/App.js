ed4461f04953c8d15a0f00af017557d1860a466d
MITGOUTHAM <mitgoutham789@gmail.com>
2023-09-13 20:23:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <center>
+      <h1>hello,world!</h1>
+     </center>
+    </div>
+  );
+}
+
+export default App;
